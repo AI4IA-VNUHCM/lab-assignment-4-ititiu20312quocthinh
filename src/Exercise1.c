8806dac6a1b213353b404fc1e242ae74c9b9aba9
ititiu20312quocthinh <91869213+ititiu20312quocthinh@users.noreply.github.com>
2021-10-25 14:12:30 +0700

    exer1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,7 +17,18 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
-    
+    int c = 1, i, j;
+  
+   for (i = 0; i < n+1; i++) {
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            c = 1;
+         else
+            c = c * (i - j + 1) / j;
+         printf("%d ", c);
+      }
+      printf("\n");
+   }
 }
 
 int main(int argc, char *argv[]) {
